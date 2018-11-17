d3546b9c62423c376a83e8d98d8bba369389b354
Arnold Galovics <galovicsarnold@gmail.com>
2018-11-17 21:18:44 +0100

    Added more tests

--- src/test/groovy/io/redskap/swagger/brake/gradle/SwaggerBrakePluginTest.groovy
+++ src/test/groovy/io/redskap/swagger/brake/gradle/SwaggerBrakePluginTest.groovy
@@ -1,20 +1,21 @@
 package io.redskap.swagger.brake.gradle
 
 import org.gradle.api.GradleException
+import org.gradle.api.internal.ClosureBackedAction
 import org.gradle.testfixtures.ProjectBuilder
 import spock.lang.Specification
 
 class SwaggerBrakePluginTest extends Specification {
-    def "checkBreakingChanges is added when plugin is applied along with java plugin"() {
+    def "checkBreakingChanges task is added when plugin is applied along with java plugin"() {
         given:
         def project = ProjectBuilder.builder().withName("test-project").build()
+        project.pluginManager.apply('java')
 
         when:
-        project.pluginManager.apply('java')
         project.pluginManager.apply(SwaggerBrakePlugin)
 
         then:
-        project.tasks.findByName("checkBreakingChanges") != null
+        assert project.tasks.findByName("checkBreakingChanges") != null
     }
 
     def "Plugin fails when check task is missing"() {
@@ -26,7 +27,102 @@ class SwaggerBrakePluginTest extends Specification {
 
         then:
         GradleException e = thrown()
-        e.getCause().message.contains('applied')
-        e.getCause().message.contains('check')
+        assert e.getCause().message.contains('applied')
+        assert e.getCause().message.contains('check')
+    }
+
+    def "Plugin defaults are applied properly"() {
+        given:
+        def project = ProjectBuilder.builder().withName("test-project").build()
+        project.group = 'group.test'
+        project.pluginManager.apply('java')
+
+        when:
+        project.pluginManager.apply(SwaggerBrakePlugin)
+        project.extensions.configure(SwaggerBrakeExtension, new ClosureBackedAction<SwaggerBrakeExtension>({
+            mavenRepoUrl = 'something'
+            newApi = 'somethingElse'
+        }))
+
+        then:
+        def extension = project.extensions.findByName("swaggerBrake")
+        assert extension != null
+        assert extension.groupId.get() == project.group
+        assert extension.artifactId.get() == project.name
+        assert extension.outputFilePath.get().startsWith("${project.buildDir}/swagger-brake")
+        assert extension.outputFormat.get() == 'HTML'
+    }
+
+    def "Plugin properties can be set with simple strings"() {
+        given:
+        def project = ProjectBuilder.builder().withName("test-project").build()
+        project.group = 'group.test'
+        project.pluginManager.apply('java')
+
+        def expectedMavenRepoUrl = 'something'
+        def expectedNewApi = 'somethingElse'
+        def expectedGroupId = 'simple-group'
+        def expectedArtifactId = 'artifact-id'
+        def expectedOutputFilePath = 'output-path'
+        def expectedOutputFormat = 'JSON'
+
+
+        when:
+        project.pluginManager.apply(SwaggerBrakePlugin)
+        project.extensions.configure(SwaggerBrakeExtension, new ClosureBackedAction<SwaggerBrakeExtension>({
+            mavenRepoUrl = expectedMavenRepoUrl
+            newApi = expectedNewApi
+            groupId = expectedGroupId
+            artifactId = expectedArtifactId
+            outputFilePath = expectedOutputFilePath
+            outputFormat = expectedOutputFormat
+        }))
+
+        then:
+        def extension = project.extensions.findByName("swaggerBrake")
+        assert extension != null
+        assert extension.mavenRepoUrl.get() == expectedMavenRepoUrl
+        assert extension.newApi.get() == expectedNewApi
+        assert extension.groupId.get() == expectedGroupId
+        assert extension.artifactId.get() == expectedArtifactId
+        assert extension.outputFilePath.get() == expectedOutputFilePath
+        assert extension.outputFormat.get() == expectedOutputFormat
+    }
+
+
+    def "Plugin properties can be set with gstrings"() {
+        given:
+        def project = ProjectBuilder.builder().withName("test-project").build()
+        project.group = 'group.test'
+        project.pluginManager.apply('java')
+
+        def expectedMavenRepoUrl = "${project.name}/something"
+        def expectedNewApi = "${project.buildDir}/swagger.json"
+        def expectedGroupId = "${project.group}"
+        def expectedArtifactId = "${project.name}"
+        def expectedOutputFilePath = "${project.buildDir}/swagger-brake/"
+        def expectedOutputFormat = "JSON"
+
+
+        when:
+        project.pluginManager.apply(SwaggerBrakePlugin)
+        project.extensions.configure(SwaggerBrakeExtension, new ClosureBackedAction<SwaggerBrakeExtension>({
+            mavenRepoUrl = expectedMavenRepoUrl
+            newApi = expectedNewApi
+            groupId = expectedGroupId
+            artifactId = expectedArtifactId
+            outputFilePath = expectedOutputFilePath
+            outputFormat = expectedOutputFormat
+        }))
+
+        then:
+        def extension = project.extensions.findByName("swaggerBrake")
+        assert extension != null
+        assert extension.mavenRepoUrl.get() == expectedMavenRepoUrl
+        assert extension.newApi.get() == expectedNewApi
+        assert extension.groupId.get() == expectedGroupId
+        assert extension.artifactId.get() == expectedArtifactId
+        assert extension.outputFilePath.get() == expectedOutputFilePath
+        assert extension.outputFormat.get() == expectedOutputFormat
     }
 }
